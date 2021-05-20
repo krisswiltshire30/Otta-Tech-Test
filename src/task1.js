5e5da76e519669ef17853a6c824bc6fce25d6dc3
krisswiltshirepz <kriss@parentzone.org.uk>
2021-05-20 15:51:36 +0100

    Find all users liked jobs + add to object array

--- src/task1.js
+++ src/task1.js
@@ -10,15 +10,28 @@ const task1 = async () => {
 
     const compareUserIds = (item) => {
         return item.user_id === allLikedJobs[i].user_id;
-      };
-    
-      // Find all unique user_ids
-      for (i = 0; i < allLikedJobs.length; i++) {
+    };
+
+    // Find all unique user_ids
+    for (i = 0; i < allLikedJobs.length; i++) {
         if (!uniqueUserIds.find(compareUserIds)) {
-          allLikedJobs[i].likedJobs = [];
-          uniqueUserIds.push(allLikedJobs[i]);
+            allLikedJobs[i].likedJobs = [];
+            uniqueUserIds.push(allLikedJobs[i]);
         }
-      }
+    }
+
+    // Find all users liked jobs.
+    // Add all of a users liked jobs to their
+    // corresponding object, as an array
+    for (i = 0; i < uniqueUserIds.length; i++) {
+        let filtered = allLikedJobs.filter((item) => {
+            return item.user_id === uniqueUserIds[i].user_id;
+        });
+
+        filtered.forEach((item) => {
+            uniqueUserIds[i].likedJobs.push(item.job_id);
+        });
+    }
 
     console.log(uniqueUserIds);
 }
