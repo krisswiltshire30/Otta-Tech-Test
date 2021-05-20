5c8924506dd90f6c39a1d3f0460990f93f4106de
krisswiltshirepz <kriss@parentzone.org.uk>
2021-05-20 15:37:01 +0100

    Get all reactions data from json

--- src/task1.js
+++ src/task1.js
@@ -1,1 +1,7 @@
-console.log('hello world');
+const task1 = async () => {
+    const getAllReactions = await fetch("../data/json/jobs.json")
+    const allReactionsJSON = await getAllReactions.json()
+    console.log(allReactionsJSON);
+}
+
+task1()
