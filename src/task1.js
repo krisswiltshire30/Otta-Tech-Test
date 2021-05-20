0d77f33c622f80f5cdf10f6ced09259e83d6b0c2
krisswiltshirepz <kriss@parentzone.org.uk>
2021-05-20 15:43:28 +0100

    Get all liked jobs

--- src/task1.js
+++ src/task1.js
@@ -2,7 +2,12 @@ const task1 = async () => {
     const getAllReactions = await fetch("../data/json/reactions.json")
     const allReactionsJSON = await getAllReactions.json()
 
-    console.log(allReactionsJSON);
+    // Get all elements with jobs that have been liked
+    const allLikedJobs = allReactionsJSON.filter((item) => {
+        return item.direction === "true";
+    });
+
+    console.log(allLikedJobs);
 }
 
 task1()
