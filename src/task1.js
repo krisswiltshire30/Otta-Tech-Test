61eaf1ab426f950c2707ff46dc82170892bdbedc
krisswiltshirepz <kriss@parentzone.org.uk>
2021-05-20 15:42:32 +0100

    Correct type in fetch string

--- src/task1.js
+++ src/task1.js
@@ -1,6 +1,7 @@
 const task1 = async () => {
-    const getAllReactions = await fetch("../data/json/jobs.json")
+    const getAllReactions = await fetch("../data/json/reactions.json")
     const allReactionsJSON = await getAllReactions.json()
+
     console.log(allReactionsJSON);
 }
 
