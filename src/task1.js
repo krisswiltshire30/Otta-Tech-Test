32d0f924ddfa85d9004e58eac0ac7f62fe88b0ac
krisswiltshirepz <kriss@parentzone.org.uk>
2021-05-20 15:45:44 +0100

    Get all unique user_ids

--- src/task1.js
+++ src/task1.js
@@ -1,4 +1,5 @@
 const task1 = async () => {
+    const uniqueUserIds = []
     const getAllReactions = await fetch("../data/json/reactions.json")
     const allReactionsJSON = await getAllReactions.json()
 
@@ -7,7 +8,19 @@ const task1 = async () => {
         return item.direction === "true";
     });
 
-    console.log(allLikedJobs);
+    const compareUserIds = (item) => {
+        return item.user_id === allLikedJobs[i].user_id;
+      };
+    
+      // Find all unique user_ids
+      for (i = 0; i < allLikedJobs.length; i++) {
+        if (!uniqueUserIds.find(compareUserIds)) {
+          allLikedJobs[i].likedJobs = [];
+          uniqueUserIds.push(allLikedJobs[i]);
+        }
+      }
+
+    console.log(uniqueUserIds);
 }
 
 task1()
