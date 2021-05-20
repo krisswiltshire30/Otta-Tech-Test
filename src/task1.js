4d061733949ef96dc2016b93cad61ee03f120677
krisswiltshirepz <kriss@parentzone.org.uk>
2021-05-20 16:03:27 +0100

    Add function to count similarities between 2 arrays + test

--- src/task1.js
+++ src/task1.js
@@ -1,7 +1,7 @@
 const task1 = async () => {
-    const uniqueUserIds = []
     const getAllReactions = await fetch("../data/json/reactions.json")
     const allReactionsJSON = await getAllReactions.json()
+    const uniqueUserIds = []
 
     // Get all elements with jobs that have been liked
     const allLikedJobs = allReactionsJSON.filter((item) => {
@@ -38,7 +38,30 @@ const task1 = async () => {
         return b.likedJobs.length - a.likedJobs.length;
     });
 
+    // Compare 2 arrays and count the number of similarities
+    const similarityHandler = (arrayA, arrayB) => {
+        let matches = 0;
+        for (i = 0; i < arrayA.length; i++) {
+            if (arrayB.indexOf(arrayA[i]) != -1)
+                matches++;
+        }
+        return matches;
+    }
 
+
+    console.log("should return 3"); 
+    console.log(similarityHandler([1,2,3],[1,2,3])); 
+    console.log("should return 3"); 
+    console.log(similarityHandler([1,2,3,4,5,6],[1,2,3])); 
+    console.log("should return 2"); 
+    console.log(similarityHandler([1,2],[1,2,3])); 
+    console.log("should return 0"); 
+    console.log(similarityHandler([1,2,3],[4,5,6])); 
+
+
+
+
+    console.log("Task1");
     console.log(uniqueUserIds);
 }
 
